d93b813d40f945b58d786b12af279f5319b2c19e
Fadli <fadliadadakholqi15@gmail.com>
2022-11-27 11:12:36 +0700

    create user api assertion

---
+++ Create-user-Assertion.js
@@ -0,0 +1,18 @@
+describe('Create users', () => {
+    it('Add a new user', () => {
+        var user = {
+            "name": 'Fadli Amada',
+            "job": 'QA Automation'
+        }
+        cy.request('POST', 'https://reqres.in/api/users', user).then((response) => {
+            expect(response.status).equal(201)
+        }).as('post')
+        cy.get('@post').its('status').should('equal', 201)
+        expect('@post').to.deep.equal('@post')
+        expect('@post').to.deep.equal('@post')
+        // expect(obj).to.deep.equal({ name: 'Fadli Amada' })
+    //     cy.get('@post').should('deep.equal', {
+    //         name: 'Fadli Amada'
+    //     });
+    });
+});
