57cad67dcb134d039de08320a5eb63678717cae2
Fadli <fadliadadakholqi15@gmail.com>
2022-12-04 14:44:05 +0700

    upload tugas script real product apk Happy Day Shopping

--- Happydayshopping.java
+++ Happydayshopping.java
@@ -17,96 +17,98 @@ import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
 
-Mobile.startApplication('C:\\Users\\Thinkpad\\Downloads\\Happy Day Shopping.apk', false)
+Mobile.startApplication('D:\\Eduwork\\KatalonStudio\\HappyDayShopping.apk',true)
 
-Mobile.delay(5, FailureHandling.STOP_ON_FAILURE)
+Mobile.delay(5,FailureHandling.STOP_ON_FAILURE)
 
-Mobile.tap(findTestObject('APK/android.view.ViewGroup1'), 0)
+Mobile.tap(findTestObject('Object Repository/android.view.ViewGroup1'),0)
 
-Mobile.tap(findTestObject('APK/android.view.ViewGroupP1'), 0)
+Mobile.tap(findTestObject('Object Repository/android.view.ViewGroupP1'),0)
 
-Mobile.tap(findTestObject('APK/android.view.ViewGroupADD'), 0)
+Mobile.tap(findTestObject('Object Repository/android.view.ViewGroupADD'),0)
 
-Mobile.tap(findTestObject('APK/android.view.ViewGroupBCK'), 0)
+Mobile.tap(findTestObject('Object Repository/android.view.ViewGroupBCK'),0)
 
-Mobile.tap(findTestObject('APK/android.view.ViewGroupp2'), 0)
+Mobile.tap(findTestObject('Object Repository/android.view.ViewGroupp2'),0)
 
-Mobile.tap(findTestObject('APK/android.view.ViewGroupADD2'), 0)
+Mobile.tap(findTestObject('Object Repository/android.view.ViewGroupADD2'),0)
 
-Mobile.tap(findTestObject('APK/android.view.ViewGroupBCK'), 0)
+Mobile.tap(findTestObject('Object Repository/android.view.ViewGroupBCK'),0)
 
-Mobile.tap(findTestObject('APK/android.view.ViewGroupP3'), 0)
+Mobile.tap(findTestObject('Object Repository/android.view.ViewGroupP3'),0)
 
-Mobile.tap(findTestObject('APK/android.view.ViewGroupADD2'), 0)
+Mobile.tap(findTestObject('Object Repository/android.view.ViewGroupADD2'),0)
 
-Mobile.tap(findTestObject('APK/android.view.ViewGroupBCK'), 0)
+Mobile.tap(findTestObject('Object Repository/android.view.ViewGroupBCK'),0)
 
-Mobile.tap(findTestObject('APK/android.view.ViewGroupP4'), 0)
+Mobile.tap(findTestObject('Object Repository/android.view.ViewGroupP4'),0)
 
-Mobile.tap(findTestObject('APK/android.view.ViewGroupADD2'), 0)
+Mobile.tap(findTestObject('Object Repository/android.view.ViewGroupADD2'),0)
 
-Mobile.tap(findTestObject('APK/android.view.ViewGroupBCK'), 0)
+Mobile.tap(findTestObject('Object Repository/android.view.ViewGroupBCK'),0)
 
-Mobile.scrollToText('palabok', FailureHandling.STOP_ON_FAILURE)
+Mobile.scrollToText('palabok',FailureHandling.STOP_ON_FAILURE)
 
-Mobile.tap(findTestObject('APK/android.view.ViewGroupP5'), 0)
+Mobile.tap(findTestObject('Object Repository/android.view.ViewGroupP5'),0)
 
-Mobile.tap(findTestObject('APK/android.view.ViewGroupADD2'), 0)
+Mobile.tap(findTestObject('Object Repository/android.view.ViewGroupADD2'),0)
 
-Mobile.tap(findTestObject('APK/android.view.ViewGroupBCK'), 0)
+Mobile.tap(findTestObject('Object Repository/android.view.ViewGroupBCK'),0)
 
-Mobile.scrollToText('NMF', FailureHandling.STOP_ON_FAILURE)
+Mobile.scrollToText('NMF',FailureHandling.STOP_ON_FAILURE)
 
-Mobile.tap(findTestObject('APK/android.view.ViewGroupP6'), 0)
+Mobile.tap(findTestObject('Object Repository/android.view.ViewGroupP6'),0)
 
-Mobile.tap(findTestObject('APK/android.view.ViewGroupADD2'), 0)
+Mobile.tap(findTestObject('Object Repository/android.view.ViewGroupADD2'),0)
 
-Mobile.tap(findTestObject('APK/android.view.ViewGroupBCK'), 0)
+Mobile.tap(findTestObject('Object Repository/android.view.ViewGroupBCK'),0)
 
-Mobile.scrollToText('Tea', FailureHandling.STOP_ON_FAILURE)
+Mobile.scrollToText('Tea',FailureHandling.STOP_ON_FAILURE)
 
-Mobile.tap(findTestObject('APK/android.view.ViewGroupP7'), 0)
+Mobile.tap(findTestObject('Object Repository/android.view.ViewGroupP7'),0)
 
-Mobile.tap(findTestObject('APK/android.view.ViewGroupADD2'), 0)
+Mobile.tap(findTestObject('Object Repository/android.view.ViewGroupADD2'),0)
 
-Mobile.tap(findTestObject('APK/android.view.ViewGroupBCK'), 0)
+Mobile.tap(findTestObject('Object Repository/android.view.ViewGroupBCK'),0)
 
-Mobile.scrollToText('sulfur', FailureHandling.STOP_ON_FAILURE)
+Mobile.scrollToText('sulfur',FailureHandling.STOP_ON_FAILURE)
 
-Mobile.tap(findTestObject('APK/android.view.ViewGroupP8'), 0)
+Mobile.tap(findTestObject('Object Repository/android.view.ViewGroupP8'),0)
 
-Mobile.tap(findTestObject('APK/android.view.ViewGroupADD2'), 0)
+Mobile.tap(findTestObject('Object Repository/android.view.ViewGroupADD2'),0)
 
-Mobile.tap(findTestObject('APK/android.view.ViewGroupBCK'), 0)
+Mobile.tap(findTestObject('Object Repository/android.view.ViewGroupBCK'),0)
 
-Mobile.scrollToText('magic', FailureHandling.STOP_ON_FAILURE)
+Mobile.scrollToText('magic',FailureHandling.STOP_ON_FAILURE)
 
-Mobile.tap(findTestObject('APK/android.view.ViewGroupP9'), 0)
+Mobile.tap(findTestObject('Object Repository/android.view.ViewGroupP9'),0)
 
-Mobile.tap(findTestObject('APK/android.view.ViewGroupADD8'), 0)
+Mobile.tap(findTestObject('Object Repository/android.view.ViewGroupADD8'),0)
 
-Mobile.tap(findTestObject('APK/android.view.ViewGroupBCK'), 0)
+Mobile.tap(findTestObject('Object Repository/android.view.ViewGroupBCK'),0)
 
-Mobile.scrollToText('katinko', FailureHandling.STOP_ON_FAILURE)
+Mobile.scrollToText('katinko',FailureHandling.STOP_ON_FAILURE)
 
-Mobile.tap(findTestObject('APK/android.view.ViewGroupP10'), 0)
+Mobile.tap(findTestObject('Object Repository/android.view.ViewGroupP10'),0)
 
-Mobile.tap(findTestObject('APK/android.view.ViewGroupADD'), 0)
+Mobile.tap(findTestObject('Object Repository/android.view.ViewGroupADD'),0)
 
-Mobile.tap(findTestObject('APK/android.view.ViewGroupBCK'), 0)
+Mobile.tap(findTestObject('Object Repository/android.view.ViewGroupBCK'),0)
 
-Mobile.scrollToText('Belo', FailureHandling.STOP_ON_FAILURE)
+Mobile.scrollToText('Belo',FailureHandling.STOP_ON_FAILURE)
 
-Mobile.tap(findTestObject('APK/android.view.ViewGroupP11'), 0)
+Mobile.tap(findTestObject('Object Repository/android.view.ViewGroupP11'),0)
 
-Mobile.tap(findTestObject('APK/android.view.ViewGroupADD6'), 0)
+Mobile.tap(findTestObject('Object Repository/android.view.ViewGroupADD6'),0)
 
-Mobile.tap(findTestObject('APK/android.view.ViewGroupBCK'), 0)
+Mobile.tap(findTestObject('Object Repository/android.view.ViewGroupBCK'),0)
 
-Mobile.tap(findTestObject('APK/android.view.ViewGroupA'), 0)
+Mobile.tap(findTestObject('Object Repository/android.view.ViewGroupA'),0)
 
-Mobile.tap(findTestObject('APK/android.view.ViewGroupC'), 0)
+Mobile.tap(findTestObject('Object Repository/android.view.ViewGroupC'),0)
 
-Mobile.tap(findTestObject('APK/android.view.ViewGroupY'), 0)
+Mobile.tap(findTestObject('Object Repository/android.view.ViewGroupY'),0)
+
+Mobile.closeApplication()android.view.ViewGroupY'), 0)
 
 Mobile.closeApplication()
